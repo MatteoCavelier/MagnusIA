d4e1b8774219c549671aa3219425bac6fedc2b5f
Zectalier <91257712+Zectalier@users.noreply.github.com>
2025-11-05 16:53:54 +0100

    Request fix

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import mlflow
+import pandas as pd
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -6,15 +7,16 @@ app = Flask(__name__)
 # 🔹 Charger le modèle pickle
 print("model loading")
 # 🔹 Charger le modèle pickle
-model = mlflow.sklearn.load_model("../res/mlflow_model")
+model = mlflow.sklearn.load_model("C:\School\MagnusIA\models\duration-5-False\mlflow_model")
 print("model loaded")
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    print("toto")
     data = request.get_json()
-    preds = model.predict([data])
+    # Convert JSON to pandas DataFrame with one row
+    df = pd.DataFrame([data])
+    preds = model.predict(df)
     return jsonify({"prediction": preds.tolist()})
 
 
